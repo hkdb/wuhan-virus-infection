a3901449e2e37e9eb61ce90ffb8516b97eb0674a
hkdb <hkdb@3df.io>
2020-03-28 10:44:16 +0000

    Translate columns to english and added credits

--- src/js/main.js
+++ src/js/main.js
@@ -70,41 +70,41 @@ $(async () => {
     height: 800,
     showFooter: true,
     columns: [{
-      title: "排名", width: 50,
+      title: "Ranking", width: 50,
       formatter: (val, row, index) => index+1,
     }, {
-      title: "國家/地區", field: "region", width: 300,
+      title: "Country/Region", field: "region", width: 300,
       footerFormatter: (data) => "總計",
     }, {
-      title: "人口(萬)", field: "population", width: 100, align: "right", sortable: true,
-      formatter: (val) => val ? numeral(val/10000).format(val >= 10000 ? "0,0" : "0.00") : "---",
+      title: "Population (k)", field: "population", width: 100, align: "right", sortable: true,
+      formatter: (val) => val ? numeral(val/1000).format(val >= 1000 ? "0,0" : "0.00") : "---",
       footerFormatter: (data) => numeral(totalNum(data, "population")/10000).format("0,0"),
     }, {
-      title: "感染人數", field: "confirmed", width: 100, align: "right", sortable: true,
+      title: "Cases", field: "confirmed", width: 100, align: "right", sortable: true,
       formatter: (val) => numeral(val).format("0,0"),
       footerFormatter: (data) => numeral(totalNum(data, "confirmed")).format("0,0"),
     }, {
-      title: "感染率(每萬)", field: "confirmedRate", width: 100, align: "right", sortable: true,
+      title: "Confirmed Ratio", field: "confirmedRate", width: 100, align: "right", sortable: true,
       formatter: (val, row) => row.population ? numeral(val*10000).format("0,0.000") : "---",
       footerFormatter: (data) => numeral(totalRate(data, "confirmed", "population")*10000).format("0,0.000"),
     }, {
-      title: "死亡人數", field: "death", width: 100, align: "right", sortable: true, 
+      title: "Death(s)", field: "death", width: 100, align: "right", sortable: true, 
       formatter: (val) => numeral(val).format("0,0"),
       footerFormatter: (data) => numeral(totalNum(data, "death")).format("0,0"),
     }, {
-      title: "死亡率", field: "deathRate", width: 100, align: "right", sortable: true,
+      title: "Death Ratio", field: "deathRate", width: 100, align: "right", sortable: true,
       formatter: (val) => numeral(val).format("0.000%"),
       footerFormatter: (data) => numeral(totalRate(data, "death", "confirmed")).format("0.000%"),
     }, {
-      title: "康復人數", field: "recovered", width: 100, align: "right", sortable: true,
+      title: "Recovered", field: "recovered", width: 100, align: "right", sortable: true,
       formatter: (val) => numeral(val).format("0,0"),
       footerFormatter: (data) => numeral(totalNum(data, "recovered")).format("0,0"),
     }, {
-      title: "康復率", field: "recoveredRate", width: 100, align: "right", sortable: true,
+      title: "Recovery Ratio", field: "recoveredRate", width: 100, align: "right", sortable: true,
       formatter: (val) => numeral(val).format("0.000%"),
       footerFormatter: (data) => numeral(totalRate(data, "recovered", "confirmed")).format("0.000%"),
     }, {
-      title: "移除", align: "center",
+      title: "Remove", align: "center",
       events: filterEvents,
       formatter: (val, row) => '<a class="remove badge  badge-danger" href="javascript:void(0)">&times</a>',
     }],
